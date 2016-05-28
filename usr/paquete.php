c250e582df0162089a7266bb0728332886032b12
RicardoLara <richard-o95@hotmail.com>
2016-05-28 16:52:45 -0700

    Paquete.php Sin Acabar
    Aún no está terminado, tengo un pedo existencial

---
+++ usr/paquete.php
@@ -0,0 +1,77 @@
+<?php
+require("db.class.php");
+session_start();
+$title = "Little Ulises Pizza&trade; - Paquetes";
+$css = "./css/paquetes.css";
+if(isset($_SESSION['email'])) {
+    $db = new database();
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<script type="text/javascript">
+		function parcear(){
+			
+		}
+		</script>
+	</head>
+	<body>
+	<p> RECIBIR EL ID DEL PAQUETE. Modificar linea 22. Y linea 5 - 6. </p>
+	<select>
+	  <option value="volvo">Volvo</option>
+	  <option value="saab">Saab</option>
+	  <option value="opel">Opel</option>
+	  <option value="audi">Audi</option>
+	</select>
+	<?php
+          $db -> query('SELECT nombre FROM paquetes WHERE id_paquete=:id_paq');
+          //Estoy suponiendo que recibo el id del paquete con GET
+          $db -> bind(':id_paq', $_GET['id_paquete']);
+          $row = $db->single();
+          
+          //Para mostrar todas las pizzas grandes
+          for ($i = 0; $i < $row['pizza_grande']; $i++) {
+		    echo 'Pizza Grande: <br/>';
+		    $db -> query('SELECT * FROM especialidad');
+		    $result = $db->resultset();
+		    echo '<select>';
+		    foreach ($result as $index => $aux)
+      			echo '<option>'.$aux['nombre'].'</option>';
+      		echo '</select><br/>';
+		  }
+
+		  //Para mostrar todas las pizzas medianas
+          for ($i = 0; $i < $row['pizza_mediana']; $i++) {
+		    echo 'Pizza Mediana: <br/>';
+		    $db -> query('SELECT * FROM especialidad');
+		    $result = $db->resultset();
+		    echo '<select>';
+		    foreach ($result as $index => $aux)
+      			echo '<option>'.$aux['nombre'].'</option>';
+      		echo '</select><br/>';
+		  }
+
+		  //Para mostrar todas las pizzas chicas
+          for ($i = 0; $i < $row['pizza_chica']; $i++) {
+		    echo 'Pizza Personal: <br/>';
+		    $db -> query('SELECT * FROM especialidad');
+		    $result = $db->resultset();
+		    echo '<select>';
+		    foreach ($result as $index => $aux)
+      			echo '<option>'.$aux['nombre'].'</option>';
+      		echo '</select><br/>';
+		  }
+
+		  if($row['refrescos'] > 0)
+		  	echo 'Este paquete incluye '.$row['refrescos'].' refrescos<br/>';
+		  else
+		  	echo 'Este paquete no incluye refrescos<br/>';
+		  echo '<button type="button" onclick="parcear()">Agregar Paquete</button>'
+    ?>
+	</body>
+</html>
+<?php
+}else{
+    echo '<script> window.location="login.php"; </script>';
+}
+?>
