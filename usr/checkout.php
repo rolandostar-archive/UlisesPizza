b6145cd819179661210d41b7fbf62140fe5cef3c
Mauricio Saavedra <masavi2008@hotmail.com>
2016-05-29 23:54:43 -0500

    Agregado parte del modal a checkout.php
    Aún no está completo, falta asignar el contenido a cada label del modal.
    
    Pendiente:
    - Terminar modal
    - Corregir css

--- usr/checkout.php
+++ usr/checkout.php
@@ -11,8 +11,9 @@ if(isset($_SESSION['email'])) {
 <html lang="en">
     <?php require_once("header.php"); ?>        
         <main>
+
             <!-- Formulario General para la Página -->
-            <form>
+            <form id="checkout" name="formulario" action="" method=POST>
                 
                 <!-- Datos de Entrega -->
                 <div class="header">
@@ -22,24 +23,24 @@ if(isset($_SESSION['email'])) {
                     <div class="datos-entrega">
                         <div class="content-form">
                             <label>Nombre:</label>
-        <?php
-          $db -> query('SELECT * FROM usuarios WHERE correo=:correo AND nombre=:nombre');
-          $db->bind(':correo', $_SESSION['email']);
-          $db->bind(':nombre', $_SESSION['nombre']);
-          $row = $db->single();
-          echo '
-            <input type="text" name="nombre" value="'.$row['nombre'].'">
-            <br><br>
-            <label>Apellido:</label>
-            <input type="text" name="apellido" value="'.$row['apellido'].'">
-            <br><br>
-            <label>Teléfono:</label>
-            <input type="text" name="telefono" value="'.$row['telefono'].'">
-            <br><br>
-            <label>Correo Electrónico:</label>
-            <input type="text" name="correo" value="'.$row['correo'].'">
-          ';
-        ?>
+                              <?php
+                                $db -> query('SELECT * FROM usuarios WHERE correo=:correo AND nombre=:nombre');
+                                $db->bind(':correo', $_SESSION['email']);
+                                $db->bind(':nombre', $_SESSION['nombre']);
+                                $row = $db->single();
+                                echo '
+                                  <input type="text" name="nombre" value="'.$row['nombre'].'">
+                                  <br><br>
+                                  <label>Apellido:</label>
+                                  <input type="text" name="apellido" value="'.$row['apellido'].'">
+                                  <br><br>
+                                  <label>Teléfono:</label>
+                                  <input type="text" name="telefono" value="'.$row['telefono'].'">
+                                  <br><br>
+                                  <label>Correo Electrónico:</label>
+                                  <input type="text" name="correo" value="'.$row['correo'].'">
+                                ';
+                              ?>
                             <br>
                         </div>
                     </div>
@@ -47,13 +48,13 @@ if(isset($_SESSION['email'])) {
 
                 <!-- Direccion de Entrega -->
                 <div class="header">
-                    <h2 class=header-checkout>Dirección de Entrega</h2>
+                    <h2 class="header-checkout">Dirección de Entrega</h2>
                 </div>
 
                 <div class="content-div">
                     <div class="direccion-left">
                         <p>Introduce la dirección completa de entrega:</p>
-                        <textarea class="text-area"></textarea>
+                        <textarea class="text-area" name="direccion"></textarea>
                         <br><br>
                         <input type="submit" value="Actualizar Mapa" />
                     </div>
@@ -62,9 +63,8 @@ if(isset($_SESSION['email'])) {
                     </div>
                 </div>
 
-                <!-- Comentarios -->
                 <div class="header">
-                    <h2 class=header-checkout>Comentarios</h2>
+                    <h2 class="header-checkout">Comentarios</h2>
                 </div>
                 <div class="container-narrow comments">
                     <textarea rows="6" cols="80"></textarea>
@@ -72,7 +72,7 @@ if(isset($_SESSION['email'])) {
 
                 <!-- Forma de Pago -->
                 <div class="header">
-                    <h2 class=header-checkout>Forma de Pago</h2>
+                    <h2 class="header-checkout">Forma de Pago</h2>
                 </div>
                 <div class="content-div">
                     <div class="pago-left"></div>
@@ -87,19 +87,89 @@ if(isset($_SESSION['email'])) {
                             <i class="fa fa-cc-paypal" style="font-size:24px;"></i>
                             <input type="radio" name="pago" value="paypal"> 
                             Paypal (Proceder a pantalla de pago)<br><br>
-                            <div class="submit-checkout">
-                            <input type="submit" value="Confirmar Pedido">
-                            </div>
                         </div>
                     </div>
                 </div>
-
             </form>
+
+            <div class="submit-checkout">
+              <button class="btn" id="myBtn">Confirmar</button>
+            </div>
+            
+            <!-- The Modal -->
+            <div id="myModal" class="modal">
+
+              <!-- Modal content -->
+              <div class="modal-content">
+                <span class="close">x</span>
+
+                <!-- Confirmación de Información de Pago -->
+                  <h3 class="header-secciones">Confirma tu Información</h3><hr>
+                  <div class="content-div-finalizar">
+                    <div class="finalizar-left">
+                      <label>Nombre:</label><br><br>
+                      <label>Apellido:</label><br><br>
+                      <label>Teléfono:</label><br><br>
+                      <label>Correo Electrónico:</label><br><br>
+                      <label>Dirección de Entrega:</label><br><br>
+                      <b><label>Forma de Pago:</label></b>
+                    </div>
+                    <div class="finalizar-right">
+                      <label name="confirmar">Introduce tu nombre</label><br><br>
+                      <label name="confirmar">Introduce tu apellido</label><br><br>
+                      <label name="confirmar">Introduce un teléfono</label><br><br>
+                      <label name="confirmar">Introduce un correo electrónico</label><br><br>
+                      <label name="confirmar">Introduce una dirección de entrega</label><br><br>
+                      <b><label name="confirmar">Elige una forma de pago</label></b>
+                    </div>
+                  </div>
+                  <div class="submit-pedido">
+                    <button class="btn" id="ordenar">Ordenar pedido</button>
+                </div>
+              </div>
+
+            </div>    
         </main>
         <div class="footer">
             &copy; Sindral Software 2016
         </div>
-    </body>
+    
+<script>
+    var modal = document.getElementById('myModal');
+    var form = document.getElementById("checkout");
+    var label = document.getElementsByName("confirmar");
+
+    document.getElementById("ordenar").onclick = function() {
+      document.forms["formulario"].submit();
+    }
+
+    // When the user clicks the button, open the modal 
+    document.getElementById("myBtn").onclick = function() {
+/*
+      label[0].innerHTML = form.elements['nombre'][form.elements['nombre'].value].nextSibling.innerHTML;
+      label[1].innerHTML = form.elements['apellido'][form.elements['apellido'].value].nextSibling.innerHTML;
+      label[2].innerHTML = form.elements['telefono'][form.elements['telefono'].value].nextSibling.innerHTML;
+      label[3].innerHTML = form.elements['correo'][form.elements['correo'].value].nextSibling.innerHTML;
+      label[4].innerHTML = form.elements['direccion'][form.elements['direccion'].value].nextSibling.innerHTML;
+      label[5].innerHTML = form.elements['pago'][form.elements['pago'].value].nextSibling.innerHTML;
+*/
+      modal.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    document.getElementsByClassName("close")[0].onclick = function() {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+
+</script>
+</body>
 </html>
 
 <?php
