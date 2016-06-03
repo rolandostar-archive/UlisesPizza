29dcb5ec3c2d2bc3089683e5be524414d7a89cc7
Mauricio Saavedra <masavi2008@hotmail.com>
2016-06-02 21:05:22 -0500

    Pantalla Gerente v 0.1
    Agregado select de sucursales para el gerente, y botón que redirigirá a
    pantalla de Agregar Empleado.
    
    Pendiente:
    * Generar la tabla con información según la sucursal del select.
    * Corregir css de botón

---
+++ dashboard/dashboard_gerente.php
@@ -0,0 +1,169 @@
+<?php
+	require_once('db.class.php');
+	$db = new database();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+	<head>
+		<meta charset="utf-8">
+		<title>Little Ulises Pizza&trade; - Dashboard</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="robots" content="index, follow">
+
+		<!-- icons -->
+		<link rel="shortcut icon" href="/favicon.ico">
+
+		<!-- Override CSS file - add your own CSS rules -->
+		<link rel="stylesheet" href="/css/styles.css">
+
+		<style>
+
+			body {
+				background: none;
+			}
+
+			.sucursal {
+				padding-top: 0.5em;
+				float: left;
+			}
+
+			.boton {
+				float: right;
+				text-align: right;
+			}
+
+			.tabla-gerente {
+				padding-top: 2em;
+				margin: 0 auto;
+				width: 100%;
+			}
+
+			.tabla-gerente th {
+				background: #E38F27;
+			}
+
+			.tabla-gerente td{
+				text-align: center;
+			}
+
+			tr:nth-child(odd) {
+				background-color: #f2f2f2
+			}
+
+		</style>
+	</head>
+
+	<body>
+		<header>
+			<div class="nav-bar">
+				<div class="container">
+					<ul class="nav">
+						<li>¡Bienvenido Juan!</li>
+						<li><a href="./login.php">Salir</a></li>
+					</ul>
+				</div>
+			</div>
+		</header>
+
+		<main class="content">
+			<div class="container">
+
+				<!-- Select de Sucursal -->
+				<div class="sucursal">
+					<select id="sucursal">
+			          <?php
+				          $db -> query('SELECT id_sucursal, nombre FROM sucursales');
+				          $result = $db->resultset();
+				          foreach ($result as $index => $row) {
+						  	echo "<option value=\"".$row['id_sucursal'].'">'.$row['nombre']."</option>\n\r";
+				          }
+			          ?>
+					</select>
+				</div>
+
+				<!-- Botón Agregar Empleado -->
+				<div class="boton">
+					<a class="btn" href="">Agregar Empleado</a>
+				</div>
+
+				<div>
+					<table class="tabla-gerente">
+						<thead>
+							<tr>
+								<th>Codigo</th>
+								<th>Tiempo Pedido</th>
+								<th>Tiempo Entrega</th>
+								<th>Empleado</th>
+								<th>Calificación</th>
+								<th>Precio</th>
+								<th>Estado</th>
+								<th>Otra Cosa</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td>1</td>
+								<td>11:59</td>
+								<td>12:26</td>
+								<td>Juan Escutia</td>
+								<td>* * * * *</td>
+								<td>$159</td>
+								<td>Entregada</td>
+								<td>Otra cosa</td>
+							</tr>
+							<tr>
+								<td>2</td>
+								<td>11:59</td>
+								<td>12:26</td>
+								<td>Juan Escutia</td>
+								<td>* * * * *</td>
+								<td>$159</td>
+								<td>Entregada</td>
+								<td>Otra cosa</td>
+							</tr>
+							<tr>
+								<td>3</td>
+								<td>11:59</td>
+								<td>12:26</td>
+								<td>Juan Escutia</td>
+								<td>* * * * *</td>
+								<td>$159</td>
+								<td>Entregada</td>
+								<td>Otra cosa</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</main>
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+
+		<script>
+		$(document).ready(function () {
+			$('#sucursal').change(function() {
+				console.log(this.options[this.selectedIndex].value);
+				alert('The option with value ' + $(this).val() + ' and text ' + $(this).text() + ' was selected.');
+			});
+		});
+
+		function submitForm() {
+		    var url = "rate.php"; // the script where you handle the form input.
+		    $.ajax({
+		    	type: "POST",
+		    	url: url,
+		           data: $("#1").serialize(), // serializes the form's elements.
+		           success: function(data)
+		           {
+		           		//innerhtml del div
+		               alert(data); // show response from the php script.
+		           }
+		       });
+
+		    return false; // avoid to execute the actual submit of the form.
+		};
+		</script>
+	</body>
+</html>
